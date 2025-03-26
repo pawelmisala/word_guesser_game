7981ba6f24f204bdb505ed6047a4e7a198c37a2c
pawelmisala <paweljozefmisala@gmail.com>
2025-03-27 00:33:21 +0100

    WORD GUESSER 1.0

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
+    final static String word = "CODEDEX"; //THE WORD TO GUESS
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        int tries = 0;
+        char[] correctLetters = new char[word.length()];
+
+        for (int i = 0; i < word.length(); i++) { //SETTING DEFAULT VALUES
+            correctLetters[i] = '_';
+        }
+
+        System.out.println("=== WORD GUESSER ===");
+        while (tries <= 4){
+            for (char i : correctLetters) {
+                System.out.print(i);
+            }
+            System.out.println(); //NEW LINE
+
+            System.out.print("> ");
+            String answer = input.nextLine();
+
+            if (answer.equalsIgnoreCase(word)){ //IF THE ANSWER IS CORRECT
+                System.out.println("CONGRATULATIONS! The word was: " + word);
+                tries = 5;
+            }
+            for (int i = 0; i < answer.length(); i++) {//CHECKING IF ANY LETTER MATCHES
+                    if (word.charAt(i) == answer.trim().toUpperCase().charAt(i)) {
+                        correctLetters[i] = answer.charAt(i);
+                    }
+                }
+            }
+            tries++;
+        }
     }
-}
